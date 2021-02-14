9d3392796868cd304f23ef8a50ffe58af4320eb6
Iwan van der Kleijn <iwan-rochus.van-der-kleijn@capgemini.com>
2021-02-14 19:13:59 +0100

    Basic generation

---
+++ src/JejuneCmd/Gen.fs
@@ -0,0 +1,54 @@
+﻿module JejuneCmd.Gen
+open System.IO
+open HandlebarsDotNet
+
+let load_template base_path path =
+    
+    use f = File.OpenText(Path.Combine(base_path, path))
+    Handlebars.Compile(f.ReadToEnd())
+   
+let expand_write_file entity (template: HandlebarsTemplate<obj,obj>) base_path (path: string) =
+    let _path = Path.Combine(base_path, path.Replace("{{entity}}", entity))
+    let value = dict["entity", entity]    
+    
+    let dir = Path.GetDirectoryName(_path)
+    if not (File.Exists(dir)) then        
+        Directory.CreateDirectory(dir) |> ignore
+        
+    File.WriteAllText(_path, template.Invoke(value))            
+    
+                
+    
+ 
+    
+let copyAndExpandFiles (entities: string list) (frompath: string) (topath: string) =
+    
+    let repo = load_template frompath "Data/Repositories/{{entity}}Repository.cs.hbs" 
+    let irepo = load_template frompath "Domain/RepositoryInterfaces/I{{entity}}Repository.cs.hbs" 
+    let controller = load_template frompath "Business/{{entity}}Management/Controllers/{{entity}}Controller.cs.hbs" 
+    let converter = load_template frompath "Business/{{entity}}Management/Converters/{{entity}}Converter.cs.hbs"
+     
+    let service = load_template frompath "Business/{{entity}}Management/Services/{{entity}}Service.cs.hbs"
+    let iservice = load_template frompath "Business/{{entity}}Management/Services/I{{entity}}Service.cs.hbs" 
+    
+    for entity in entities do
+        expand_write_file entity repo topath "Data/Repositories/{{entity}}Repository.cs"
+        expand_write_file entity irepo topath "Domain/RepositoryInterfaces/I{{entity}}Repository.cs"
+        expand_write_file entity controller topath "Business/{{entity}}Management/Controllers/{{entity}}Controller.cs"
+        expand_write_file entity converter topath "Business/{{entity}}Management/Converters/{{entity}}Converter.cs"
+
+        expand_write_file entity service topath "Business/{{entity}}Management/Services/{{entity}}Service.cs"
+        expand_write_file entity iservice topath "Business/{{entity}}Management/Services/I{{entity}}Service.cs"
+
+        
+(*
+Data/Repositories/{{entity}}Repository.cs
+Domain/RepositoryInterfaces/I{{entity}}Repository.cs
+
+Business/{{entity}}Management/Controllers/{{entity}}Controller.cs
+Business/{{entity}}Management/Converters/{{entity}}Converter.cs
+Business/{{entity}}Management/Dto/{{entity}}Dto.cs
+Business/{{entity}}Management/Services/{{entity}}Service.cs
+Business/{{entity}}Management/Services/I{{entity}}Service.cs
+        
+*)
--- src/JejuneCmd/Program.fs
+++ src/JejuneCmd/Program.fs
@@ -1,19 +1,29 @@
 // Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
 
 open System
-open Jejune
-
+open HandlebarsDotNet
 // Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+
+open JejuneCmd
+open Gen
+    
 
 [<EntryPoint>]
 let main argv =
     
-    //let result = Jejune.Main.expandTemplates @"C:\src\Jejune\src\Jejune\test"
-
-    Jejune.Main.expandTemplates @"C:\src\Jejune\src\Jejune\test\testdata.json" @"C:\src\Jejune\src\Jejune\test" ""
-    //printfn "Generated:  %s" result
-    0 // return an integer exit code
-
+    copyAndExpandFiles ["Test"] @"C:\src\CASO-Backend\source\Templates\WebAPI\Devon4Net.WebAPI.Implementation\template" @"C:\src\CASO-Backend\source\Templates\WebAPI\Devon4Net.WebAPI.Implementation"
+    (*if argv.Length <> 3 then
+        printf "Invalid number of arguments"
+        printf "entity_file template_path destination_path"
+        -1
+    else
+        let entityfile = argv.[0]
+        let frompath = argv.[1]
+        let topath = argv.[2]
+        //copyAndExpandFiles "test" frompath topath
+        copyAndExpandFiles ["Test"] @"C:\src\CASO-Backend\source\Templates\WebAPI\Devon4Net.WebAPI.Implementation\template" @"c:/temp"
+        0
+    *)
+    0
+    
     
